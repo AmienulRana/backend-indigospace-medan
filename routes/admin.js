056cb6522575d625431d9e6d32f33bd7498f1831
AmienulRana <amienulrana@gmail.com>
2022-06-19 14:44:26 +0700

    men-enable route registrasi

--- routes/admin.js
+++ routes/admin.js
@@ -4,22 +4,22 @@ const middleware = require('../middleware')
 const admin = (app) => {
   const cAdmin = require('../controllers/admin');
   app.route('/login').post(cAdmin.login)
+  // app.route('/registrasi').post(async(req,res) => {
+  //   let data = req.body
+  //   try{
+  //     data.password = await bcrypt.hash(req.body.password, 10);
+  //     const admin = new Admin(req.body);
+  //     await admin.save();
+  //     return res.status(200).json({message :' Success'})
+  //   }catch(err){
+  //     console.log(err);
+  //   }
+  //   res.redirect('/')
+  // })
 }
 module.exports = admin;
 
 
 // / app.route('/register').get((req,res) => {
 //   res.render('registrasi')
-// })
-// app.route('/registrasi').post(async(req,res) => {
-//   let data = req.body
-//   try{
-//     data.password = await bcrypt.hash(req.body.password, 10);
-//     const admin = new Admin(req.body);
-//     await admin.save()
-//   }catch(err){
-//     console.log(err);
-//   }
-//   res.redirect('/')
-//
 // })

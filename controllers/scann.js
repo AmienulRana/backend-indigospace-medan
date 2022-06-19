673696d84a27395da0d4597e7e980b70f649a9b9
AmienulRana <amienulrana@gmail.com>
2022-06-19 14:38:23 +0700

    mengubah kode yang sebelumnya menjadi lebih rapi dan mudah dipahami

--- controllers/scann.js
+++ controllers/scann.js
@@ -1,63 +1,49 @@
 const StartUp = require("../models/startUp");
 const Investor = require("../models/investor");
+const Event = require('../models/event');
+const { error, success } = require('../response');
 
+const updateKeterangan = (data, model, res) => {
+  const sayName = data.nama_investor || data.nama_startup;
+  const date = new Date();
+  const time = `${date.getHours()}:${date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()}`;
+  if(data.keterangan === 'Absen'){
+    model.updateOne({ _id: data._id }, { $set: { keterangan: "Hadir", waktu_hadir: time } }
+    ).then(() =>  success(res, `Selamat Datang ${sayName}`));
+  }else return error(res, 'Anda telah Hadir dalam event ini');
+}
 module.exports = {
-  getDetail: async (req, res) => {
-    const { eventId } = req.params;
+  scann: async (req, res) => {
     const { _id } = req.body;
+    const date = new Date();
+    const month = ['01', '02','03','04','05','06','07','08','09','10','11','12'];
+    const timeEvent = `${date.getFullYear()}-${month[date.getMonth()]}-${date.getDate()}`;
     try {
-      const startups = await StartUp.find({ _id_event: eventId }).then(
-        async (result) => {
-          const startup = result.filter((res) => res._id == _id);
-          if (startup.length > 0) {
-            if (startup[0].keterangan === "Absen") {
-              const updt = StartUp.updateOne(
-                { _id: _id },
-                { $set: { keterangan: "Hadir" } }
-              ).then(() => {
-                res.status(200).json({
-                  message: "Selamat Datang " + startup[0].nama_startup,
-                  error: false,
-                });
-              });
-            } else if (startup[0].keterangan === "Hadir") {
-              res.status(201).json({
-                message: "Anda telah Hadir dalam event ini",
-                error: false,
-              });
-            }
-          } else if (startup.length < 1) {
-            const investors = await Investor.find({
-              _id_event: eventId,
-            }).then(async (result) => {
-              const investor = result.filter((res) => res._id == _id);
-              if (investor.length > 0) {
-                if (investor[0].keterangan === "Absen") {
-                  const updt = Investor.updateOne(
-                    { _id: _id },
-                    { $set: { keterangan: "Hadir" } }
-                  ).then(() => {
-                    res.status(200).json({
-                      message: "Selamat Datang " + investor[0].nama_investor,
-                      error: false,
-                    });
-                  });
-                } else if (investor[0].keterangan === "Hadir") {
-                  res.status(201).json({
-                    message: "Anda telah Hadir dalam event ini",
-                    error: true,
-                  });
-                }
-              } else {
-                res.status(201).json({
-                  message: "Anda tidak terdaftar dalam Event ini",
-                  error: true,
-                });
-              }
-            });
-          }
+      const startup = await StartUp.findOne({ _id });
+      const investor = await Investor.findOne({ _id });
+      if(startup){
+        const eventStartup = await Event.findOne({_id :startup._id_event});
+        if(eventStartup.jadwal_event === timeEvent){
+          return updateKeterangan(startup, StartUp, ResizeObserverEntry)
         }
-      );
+        else if(eventStartup.jadwal_event < timeEvent || eventInvestor.jadwal_event < timeEvent){
+          return error(res, 'Event ini belum dimulai');
+        }else{
+          return error(res, 'Event ini Sudah berakhir');
+        }
+      }else if(investor){
+        const eventInvestor = await Event.findOne({_id :investor._id_event});
+        if(eventInvestor.jadwal_event === timeEvent){
+            return updateKeterangan(investor, Investor, res)
+        }
+        else if(eventStartup.jadwal_event < timeEvent || eventInvestor.jadwal_event < timeEvent){
+          return error(res, 'Event ini belum dimulai');
+        }else{
+          return error(res, 'Event ini Sudah berakhir');
+        }
+      }else{
+        return error(res, 'Anda tidak terdaftar dalam event manapun!')
+      }
     } catch (err) {
       console.log(err);
     }

f27dd3a397c6ad591d11e7681830079a11603b53
AmienulRana <amienulrana@gmail.com>
2022-06-20 19:06:09 +0700

    mengubah route scann /scann/:id => /scann

--- routes/scann.js
+++ routes/scann.js
@@ -1,7 +1,7 @@
 const middleware = require("../middleware");
 const cScann = require("../controllers/scann");
 const scann = (app) => {
-  app.route("/scann/:eventId").post(middleware, cScann.getDetail);
+  app.route("/scann").post(middleware, cScann.scann);
 };
 
 module.exports = scann;

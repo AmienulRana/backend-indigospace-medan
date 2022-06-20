e353760c1c29b87b54edba861c074c32742ce32a
AmienulRana <amienulrana@gmail.com>
2022-06-20 19:07:43 +0700

    no spesific commit(2)

--- routes/event.js
+++ routes/event.js
@@ -1,9 +1,6 @@
 const middleware = require("../middleware");
 const event = (app) => {
   const cEvent = require("../controllers/event");
-  app.route("/event-page").get((req, res) => {
-    res.render("pageEvent");
-  });
   app.route("/event").get(middleware, cEvent.getEvent);
   app.route("/event/:id").get(middleware, cEvent.DetailEvent);
   app.route("/event").post(middleware, cEvent.addEvent);
